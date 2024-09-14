7453bcdbea0b50f298a0e9c0e4d934087c3e8963
Thota Teja <teja-246@users.noreply.github.com>
2024-09-14 16:31:34 +0530

    connection to local mongo

--- src/index.js
+++ src/index.js
@@ -1,20 +1,44 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
+// require('dotenv').config({path: './env'})
+import dotenv from 'dotenv'
+import connectDB from "./db/index.js";
 
-(async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("error", (error) => {
-            console.log("ERROR: ", error);
-            throw error
-        })
-        app.listen(process.env.PORT, () => {
-            console.log(`App is listening to port ${process.env.PORT}`);
 
-        })
-    }
-    catch (error) {
-        console.log('ERROR:', error);
-        throw err
-    }
-})()
+dotenv.config({path: './env'})
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// (async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error", (error) => {
+//             console.log("ERROR: ", error);
+//             throw error
+//         })
+//         app.listen(process.env.PORT, () => {
+//             console.log(`App is listening to port ${process.env.PORT}`);
+
+//         })
+//     }
+//     catch (error) {
+//         console.log('ERROR:', error);
+//         throw err
+//     }
+// })()
+
