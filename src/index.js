7650f7d39060cc34301154991c2b76f259138e07
Thota Teja <teja-246@users.noreply.github.com>
2024-09-16 11:06:18 +0530

    minor

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import dotenv from 'dotenv'
 import connectDB from "./db/index.js";
 
 
-dotenv.config({path: './env'})
+dotenv.config({ path: './env' })
 
 
 connectDB()
+    .then(() => {
+        app.listen(process.env.port || 8000, () => {
+            console.log(`Server is running at port: ${process.env.PORT}`);
 
+        })
+    })
+    .catch((err) => {
+        console.log('MONGODB connection failed !', err);
+
+    })
 
 
 
