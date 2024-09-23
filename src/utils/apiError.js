5ec4db4b29f74e88bf355946458768e06d7d413a
Thota Teja <teja-246@users.noreply.github.com>
2024-09-23 22:10:57 +0530

    api response

--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -10,8 +10,15 @@ class ApiError extends Error {
         this.data = null
         this.message = message
         this.success = false
-        this.errors = this.errors
+        this.errors = errors
 
-        
+        if(statck){
+            this.statck = statck
+        }
+        else{
+            Error.captureStackTrace(this, this.constructor)
+        }
     }
 }
+
+export {ApiError}
