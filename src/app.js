3b3a4e9101e025dc9342a223f3be561b4637f5f3
T. Teja <tteja375@gmail.com>
2024-11-02 01:43:16 +0530

    custom api

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-import express from 'express'
+import express, { urlencoded } from "express";
+import cookieParser from "cookie-parser";
 import cors from 'cors'
-import cookieParser from 'cookie-parser'
 
 const app = express()
 
@@ -8,18 +8,11 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
-
-app.use(express.json({
-    limit: "10kb"
-}))
-
-app.use(express.urlencoded({
-    extended: true,
-    limit: "10kb"
-}))
-
-app.use(express.static("public"))
-
+app.use(express.json({limit: "16kb"}))
+app.use(urlencoded({extended: true, limit:"16kb"}))
+app.use(express.static('public'))
 app.use(cookieParser())
+
+
 
 export { app }
