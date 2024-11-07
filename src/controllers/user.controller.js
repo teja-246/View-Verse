7adc90aff1ade33c205466dbd41ec8d95e84e18e
T. Teja <tteja375@gmail.com>
2024-11-08 01:16:34 +0530

    setting user control

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,21 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler( async(req, res) =>{
-    res.status(200).json({
-        message: "ok"
-    })
+    //user details
+    // validations - not empty
+    // pre existing user (check by username and email)
+    // files exist => cloudinary
+    //create user object - - create entry in db
+    //remove and refresh token from response
+    // check for user creation
+    // return res
+
+
+    const {fullName, email, username, password} = req.body
+    console.log("email:", email);
+    
+
+
 })
 
 
